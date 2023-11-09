68b5127f2a98ccb769a9f6e93034d6b18dd43d50
can23384 <can23384@uvg.edu.gt>
2023-11-09 12:59:10 -0600

    poner publico la clase paciente

--- Paciente.java
+++ Paciente.java
@@ -1,4 +1,4 @@
-class Paciente {
+public class Paciente {
     private int id;
     private String nombre;
     private String seguroMedico;
@@ -11,7 +11,22 @@ class Paciente {
 
     // Getters y setters
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSeguro() {
+        return seguroMedico;
+    }
+
+    public int getID() {
+        return id;
+    }
+
     public String toString() {
         return "ID: " + id + "\nNombre: " + nombre + "\nSeguro Médico: " + seguroMedico;
     }
+
 }
+
+
--- registroPaciente.java
+++ registroPaciente.java
@@ -83,35 +83,4 @@ public class registroPaciente{
     }
 }
 
-//la calse paciente
-class Paciente {
-    private int id;
-    private String nombre;
-    private String seguroMedico;
 
-    public Paciente(int id, String nombre, String seguroMedico) {
-        this.id = id;
-        this.nombre = nombre;
-        this.seguroMedico = seguroMedico;
-    }
-
-    // Getters y setters
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getSeguro() {
-        return seguroMedico;
-    }
-
-    public int getID() {
-        return id;
-    }
-
-    public String toString() {
-        return "ID: " + id + "\nNombre: " + nombre + "\nSeguro Médico: " + seguroMedico;
-    }
-
-
-}
