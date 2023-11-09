31b0f88191a2be3e6f0e8e1e2fb871bee05796b9
Gabriel_Soto <g.soto4792@gmail.com>
2023-11-09 01:59:11 -0600

    Nueva clase: Calendario.java
    Contiene Arraylist objetos tipo CitaMedica

---
+++ Calendario.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calendario {
+   
+    private List<CitaMedica> citas;
+
+    public Calendario() {
+        citas = new ArrayList<>();
+
+    }
+
+    public void agregarCita(CitaMedica cita) {
+        citas.add(cita);
+
+    }
+
+    public List<CitaMedica> obtenerCitas() {
+        return citas;
+    }
+
+
+
+}
