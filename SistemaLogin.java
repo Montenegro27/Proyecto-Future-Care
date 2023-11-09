3fc73073bc92306e5a1003d8c6f1e9042e9d8479
Montenegro27 <118623623+Montenegro27@users.noreply.github.com>
2023-11-09 12:24:48 -0600

    Correcciones 2.0
    Se corrigio algunos errores del menú de pacientes, asi como la creacion de un limpiador de consola, para mejor visibilidad.

--- SistemaLogin.java
+++ SistemaLogin.java
@@ -7,17 +7,18 @@ public class SistemaLogin {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+    
         while (true) {
+            limpiarConsola();// Limpiar la consola al inicio de cada iteración
             System.out.println("Iniciar sesión:");
             System.out.print("Usuario: ");
             String usuario = scanner.next();
             System.out.print("Contraseña: ");
             String contrasena = scanner.next();
-
+    
             // Realizar la autenticación
             Usuario usuarioAutenticado = autenticar(usuario, contrasena);
-
+    
             if (usuarioAutenticado != null) {
                 if (usuarioAutenticado.getTipo().equals("medico")) {
                     mostrarMenuMedico(usuarioAutenticado);
@@ -26,6 +27,8 @@ public class SistemaLogin {
                 }
             } else {
                 System.out.println("Credenciales incorrectas. Inténtalo de nuevo.");
+                scanner.nextLine(); // Limpiar el buffer del scanner
+                scanner.nextLine(); // Esperar a que el usuario presione Enter antes de continuar
             }
         }
     }
@@ -51,6 +54,7 @@ public class SistemaLogin {
     }
     
     public static void mostrarMenuMedico(Usuario usuario) {
+        limpiarConsola();
         System.out.println("Bienvenido, Médico " + usuario.getUsuario());
         System.out.println("Especialidad: " + usuario.getEspecialidad());
         // Menú específico para médicos
@@ -60,76 +64,140 @@ public class SistemaLogin {
         System.out.println("4. Salir");
     }
 
+    
     public static void mostrarMenuPaciente(Usuario usuario) {
-        System.out.println("Bienvenido, Paciente " + usuario.getUsuario());
-        System.out.println("Número de Seguro: " + usuario.getNumSeguro());
-        System.out.println("1. Ver historial médico");
-        System.out.println("2. Mostrar información del paciente");
-        System.out.println("3. Revisar citas");
-        System.out.println("4. Salir");
-    
-        Scanner scanner = new Scanner(System.in);
-        int opcion = scanner.nextInt();
-    
-        switch (opcion) {
-            case 1:
-                break;
-            case 2:
-                historial paciente = obtenerPacientePorId(usuario.getNumSeguro());
+        while (true) {
+            limpiarConsola();
+            System.out.println("Bienvenido, Paciente " + usuario.getUsuario());
+            System.out.println("Número de ID: " + usuario.getNumSeguro());
+            System.out.println("1. Ver historial médico");
+            System.out.println("2. Mostrar información del paciente");
+            System.out.println("3. Revisar citas");
+            System.out.println("4. Salir");
 
-                if (paciente != null) {
-                    mostrarMenuHistorialMedico(paciente);
-                } else {
-                    System.out.println("No se encontró información del paciente.");
-                }
-                break;
-            case 3:
-            case 4:
-                System.exit(0);
-            default:
-                System.out.println("Opción no válida. Inténtalo de nuevo.");
+            Scanner scanner = new Scanner(System.in);
+            int opcion = scanner.nextInt();
+
+            historial paciente = obtenerPacientePorId(usuario.getNumSeguro());
+            switch (opcion) {
+                case 1:
+                    if (paciente != null) {
+                        mostrarSubMenuAntecedentes(usuario);
+                    } else {
+                        System.out.println("No se encontró información del paciente.");
+                    }
+                    break;
+                case 2:
+                    if (paciente != null) {
+                        mostrarMenuHistorialMedico(paciente);
+                    } else {
+                        System.out.println("No se encontró información del paciente.");
+                    }
+                    break;
+                case 3:
+                case 4:
+                    return; // Salir del bucle y volver al menú principal
+                default:
+                    System.out.println("Opción no válida. Inténtalo de nuevo.");
+            }
         }
     }
 
     public static void mostrarMenuHistorialMedico(historial paciente) {
-        System.out.println("Historial Médico de " + paciente.getNombre());
-        System.out.println("1. Ver nombre");
-        System.out.println("2. Ver edad");
-        System.out.println("3. Ver sexo");
-        System.out.println("4. Ver grupo sanguíneo");
-        System.out.println("5. Ver altura y peso");
-        System.out.println("6. Ver medicamento actual");
-        System.out.println("7. Ver alergias");
-
         Scanner scanner = new Scanner(System.in);
-        int opcion = scanner.nextInt();
-
-        switch (opcion) {
-            case 1:
-                System.out.println("Nombre: " + paciente.getNombre());
-                break;
-            case 2:
-                System.out.println("Edad: " + paciente.getEdad());
-                break;
-            case 3:
-                System.out.println("Sexo: " + paciente.getSexo());
-                break;
-            case 4:
-                System.out.println("Grupo Sanguíneo: " + paciente.getGrupoSanguineo());
-                break;
-            case 5:
-                System.out.println("Altura y Peso: " + paciente.getAlturaPeso());
-                break;
-            case 6:
-                System.out.println("Medicamento Actual: " + paciente.getMedicamentoActual());
-                break;
-            case 7:
-                System.out.println("Alergias: " + paciente.getAlergias());
-                break;
-            default:
-                System.out.println("Opción no válida. Inténtalo de nuevo.");
-        }
+    
+        do {
+            limpiarConsola();
+            System.out.println("Historial Médico de " + paciente.getNombre());
+            System.out.println("1. Ver edad");
+            System.out.println("2. Ver sexo");
+            System.out.println("3. Ver grupo sanguíneo");
+            System.out.println("4. Ver altura y peso");
+            System.out.println("5. Ver medicamento actual");
+            System.out.println("6. Ver alergias");
+            System.out.println("7. Volver al menú paciente");
+    
+            int opcion = scanner.nextInt();
+    
+            switch (opcion) {
+                case 1:
+                    System.out.println("Edad: " + paciente.getEdad());
+                    break;
+                case 2:
+                    System.out.println("Sexo: " + paciente.getSexo());
+                    break;
+                case 3:
+                    System.out.println("Grupo Sanguíneo: " + paciente.getGrupoSanguineo());
+                    break;
+                case 4:
+                    System.out.println("Altura y Peso: " + paciente.getAlturaPeso());
+                    break;
+                case 5:
+                    System.out.println("Medicamento Actual: " + paciente.getMedicamentoActual());
+                    break;
+                case 6:
+                    System.out.println("Alergias: " + paciente.getAlergias());
+                    break;
+                case 7:
+                    return; // Salir y volver al menú paciente
+                default:
+                    System.out.println("Opción no válida. Inténtalo de nuevo.");
+            }
+    
+            System.out.println("¿Quieres ver más información? (si/no)");
+            String respuesta = scanner.next().toLowerCase();
+    
+            if (!respuesta.equals("si")) {
+                return; // Salir si la respuesta no es "si"
+            }
+    
+        } while (true); // Repetir el bucle mientras el usuario quiera ver más información
     }
+
+    public static void mostrarSubMenuAntecedentes(Usuario usuario) {
+        Scanner scanner = new Scanner(System.in);
+    
+        do {
+            limpiarConsola();
+            System.out.println("Seleccione una opción:");
+            System.out.println("1. Ver antecedentes médicos");
+            System.out.println("2. Ver antecedentes quirúrgicos");
+            System.out.println("3. Volver al menú paciente");
+    
+            int opcionSubMenu = scanner.nextInt();
+    
+            historial paciente = obtenerPacientePorId(usuario.getNumSeguro());
+    
+            switch (opcionSubMenu) {
+                case 1:
+                    if (paciente != null) {
+                        System.out.println("Antecedentes Médicos: " + paciente.getantecedentesmed());
+                    } else {
+                        System.out.println("No se encontró información del paciente.");
+                    }
+                    break;
+                case 2:
+                    if (paciente != null) {
+                        System.out.println("Antecedentes Quirúrgicos: " + paciente.getantecedentesqui());
+                    } else {
+                        System.out.println("No se encontró información del paciente.");
+                    }
+                    break;
+                case 3:
+                    return; // Salir y volver al menú paciente
+                default:
+                    System.out.println("Opción no válida. Inténtalo de nuevo.");
+            }
+    
+            System.out.println("¿Quieres ver más información? (si/no)");
+            String respuesta = scanner.next().toLowerCase();
+    
+            if (!respuesta.equals("si")) {
+                return; // Salir si la respuesta no es "si"
+            }
+    
+        } while (true); // Repetir el bucle mientras el usuario quiera ver más información
+    }    
 
     public static historial obtenerPacientePorId(String idPaciente) {
         try (BufferedReader reader = new BufferedReader(new FileReader("historialm.csv"))) {
@@ -145,11 +213,13 @@ public class SistemaLogin {
                     String sexo = partes[3];
                     String grupoSanguineo = partes[4];
                     String alturaPeso = partes[5];
+                    String antecedentesmed = partes[6];
+                    String antecedentesqui = partes[7];
                     String medicamentoActual = partes[8];
                     String alergias = partes[9];
     
                     // Crea y devuelve un objeto historial con los datos leídos
-                    return new historial(idCSV, nombre, edad, sexo, grupoSanguineo, alturaPeso, medicamentoActual, alergias);
+                    return new historial(idCSV, nombre, edad, sexo, grupoSanguineo, alturaPeso, antecedentesmed, antecedentesqui, medicamentoActual, alergias);
                 }
             }
         } catch (IOException e) {
@@ -158,8 +228,25 @@ public class SistemaLogin {
         System.out.println("No se encontró información del paciente con ID: " + idPaciente);
         return null;
     }
-     
+
+    public static void limpiarConsola() {
+        try {
+            final String os = System.getProperty("os.name");
     
+            if (os.contains("Windows")) {
+                // Si el sistema operativo es Windows, utiliza "cls"
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                // En otros sistemas operativos, utiliza "clear"
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (final Exception e) {
+            // Maneja excepciones si ocurren al intentar limpiar la consola
+            e.printStackTrace();
+        }
+    }    
+
 }
 class Usuario {
     private String usuario;
--- historial.java
+++ historial.java
@@ -5,22 +5,25 @@ class historial {
     private String sexo;
     private String grupoSanguineo;
     private String alturaPeso;
+    private String antecedentesmed;
+    private String antecedentesqui;
     private String medicamentoActual;
     private String alergias;
 
-    public historial(String id, String nombre, String edad, String sexo, String grupoSanguineo, String alturaPeso, String medicamentoActual, String alergias) {
+    public historial(String id, String nombre, String edad, String sexo, String grupoSanguineo, String alturaPeso, String antecedentesmed, String antecedentesqui, String medicamentoActual, String alergias) {
         this.id = id;
         this.nombre = nombre;
         this.edad = edad;
         this.sexo = sexo;
         this.grupoSanguineo = grupoSanguineo;
         this.alturaPeso = alturaPeso;
+        this.antecedentesmed = antecedentesmed;
+        this.antecedentesqui = antecedentesqui;
         this.medicamentoActual = medicamentoActual;
         this.alergias = alergias;
     }
 
-    public historial(String idCSV, String string, String string2, String string3, String string4, String string5,
-            String string6, String string7, String string8, String string9) {
+    public historial(String idCSV, String string, String string2, String string3, String string4, String string5, String string6, String string7, String string8) {
     }
 
     public String getId() {
@@ -45,6 +48,14 @@ class historial {
 
     public String getAlturaPeso() {
         return alturaPeso;
+    }
+
+    public String getantecedentesmed() {
+        return antecedentesmed;
+    }
+
+    public String getantecedentesqui() {
+        return antecedentesqui;
     }
 
     public String getMedicamentoActual() {
